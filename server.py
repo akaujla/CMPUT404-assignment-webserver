a3fc0caaba53ec76217d7d19a15ee279eb14657a
akaujla <akaujla@ualberta.ca>
2022-09-30 15:19:56 -0600

    freetests.py works

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -32,6 +33,47 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
+
+        data = self.data.split(b' ')
+
+        # Check if method is GET
+        if bytes("GET", 'utf-8') == data[0]:
+            path = data[1].decode('utf-8')
+            print("Path is", path)
+
+            if path == '/':
+                self.request.send(bytes("HTTP/1.1 200 OK\r\n", 'utf-8'))
+
+            # open file
+            else:
+                try:
+                    print("www" in path)
+                    cur_dir = os.path.abspath(os.getcwd())
+                    if 'www' in path:
+                        print("cur dir type is", type(cur_dir))
+                        path = str(cur_dir) + path
+                    else:
+                        path = str(cur_dir) + "/www" + path
+
+                    print("new path is", path)
+                    file = open(path)
+                    file_type = path.split('.')[1]
+                    print("file type is", file_type)
+                    output_data = file.read()
+
+                    if file_type == 'html':
+                        self.request.send(bytes("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + str(len(output_data)) + "\r\n" + output_data, 'utf-8'))
+                    elif file_type == 'css':
+                        self.request.send(bytes("HTTP/1.1 200 OK\r\nContent-Type: text/css\r\nContent-Length: " + str(len(output_data)) + "\r\n" + output_data, 'utf-8'))
+                
+                except IOError:
+                    print("ERRRRRRRRRRRRRRRRROR")
+                    self.request.send(bytes("HTTP/1.1 404 Not Found\r\n", 'utf-8'))
+
+        else:
+            self.request.send(bytes("HTTP/1.1 405 Method Not Allowed\r\n", 'utf-8'))
+
+
         self.request.sendall(bytearray("OK",'utf-8'))
 
 if __name__ == "__main__":
@@ -44,3 +86,40 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+    
+    """ BUFFER_SIZE = 1024
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
+        #bind socket to address
+        s.bind((HOST, PORT))
+        #set to listening mode
+        s.listen(2)
+        
+        #continuously listen for connections
+        while True:
+            conn, addr = s.accept()
+            
+            print("Connected by", addr)
+            
+            try:
+                # get filename from data and read file
+                data = conn.recv(BUFFER_SIZE)
+                filename = data.split()[1]
+                file = open(filename[1:])
+                output_data = file.read()
+                
+                # send header and data
+                full_data = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " + str(len(output_data)) + "\n\n" + output_data
+                conn.send(full_data)
+                conn.close()
+            
+            # if file does not exist
+            except IOError:
+                conn.send("HTTP/1.1 404 Not Found\n")
+                conn.close() """
+
+    
+    
